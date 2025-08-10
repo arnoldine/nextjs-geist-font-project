2494f370b9a71d62c0492ad01265917e8664421d
arnoldine <awulu.lartey@gmail.com>
2025-08-10 19:49:46 +0000

    Add simply-accounting/app/__init__.py 1754855383003

---
+++ simply-accounting/app/__init__.py
@@ -0,0 +1,7 @@
+"""
+Simply Accounting - A comprehensive POS and accounting system for small businesses.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Simply Accounting Team"
+__description__ = "Web-based POS and inventory system with QuickBooks-like accounting features"
